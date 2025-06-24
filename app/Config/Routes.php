eb34029159ae2ed9839b4450274fbb29429645d7
danielaquaries20 <danielaquaries@gmail.com>
2025-06-24 15:03:22 +0700

    Added Routes for View

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+$routes->get('/', 'CompanyProfile::index');
+$routes->get('/tentang', 'CompanyProfile::about');
+$routes->get('/layanan', 'CompanyProfile::services');
+$routes->get('/kontak', 'CompanyProfile::contact');
+$routes->post('/contact/submit', 'CompanyProfile::submitContact');
