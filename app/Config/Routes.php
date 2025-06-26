9ee26776cad0ef16beaf43147b1c880c59affd89
aditiaekaramadhan <aditiaekaramadhan.dev@gmail.com>
2025-06-26 14:39:21 +0700

    Added Admin Controller and Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -21,3 +21,24 @@ $routes->get('/kontak', 'CompanyProfile::contact');
 
 // Proses submit form kontak
 $routes->post('/contact/submit', 'CompanyProfile::submitContact');
+
+
+
+// Admin Routes
+$routes->group('admin', function ($routes) {
+    $routes->get('/', 'Admin::index');
+    $routes->get('login', 'Admin::login');
+    $routes->post('authenticate', 'Admin::authenticate');
+    $routes->get('logout', 'Admin::logout');
+    $routes->get('contacts', 'Admin::contacts');
+    $routes->get('mark-read/(:num)', 'Admin::markAsRead/$1');
+    $routes->get('mark-all-read', 'Admin::markAllAsRead');
+    $routes->get('delete/(:num)', 'Admin::deleteContact/$1');
+    $routes->get('delete-all-read', 'Admin::deleteAllRead');
+    $routes->get('settings', 'Admin::settings');
+    $routes->get('services', 'Admin::services');
+    $routes->get('partners', 'Admin::partners');
+    $routes->get('initialize', 'Admin::initializeData');
+    $routes->get('change-password', 'Admin::changePassword');
+    $routes->post('update-password', 'Admin::updatePassword');
+});
